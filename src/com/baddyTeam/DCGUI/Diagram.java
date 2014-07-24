2a5918be6b3faf4f5c485cf93bbdb7ecc8a475ca
Tobal Oval <X.zaning.X@gmail.com>
2014-07-23 23:50:33 -0400

    Diagram as a Factory (Design Pattern) for the abstract diagram structure

--- src/com/baddyTeam/DCGUI/Diagram.java
+++ src/com/baddyTeam/DCGUI/Diagram.java
@@ -3,12 +3,22 @@ package com.baddyTeam.DCGUI;
 import java.util.ArrayList;
 
 
-public class Diagram {
+public class Diagram implements Iterable<NodeDC>{
+    //TODO this class will use the Fabric pattern design.
 
-	private ArrayList<NodeDC> nodes;
+	private NodeDC firstNode;
 
 	public Diagram() {
-		nodes     = new ArrayList<NodeDC>();
+		this.firstNode = null;
 	}
+
+    @Override
+    public NodeDC iterator(){
+        return this.firstNode;
+    }
+
+    public void createVarSentence(){}
+    public void createIfSentence(){}
+    public void createCSentence(){}
 
 }
