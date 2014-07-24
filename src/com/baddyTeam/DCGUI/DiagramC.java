a7400111066514a694020081e824e39c3dd7715f
Tobal Oval <X.zaning.X@gmail.com>
2014-07-23 23:57:45 -0400

    replaced current, left and right with falseNext & trueNext.

--- src/com/baddyTeam/DCGUI/DiagramC.java
+++ src/com/baddyTeam/DCGUI/DiagramC.java
@@ -4,8 +4,7 @@ package com.baddyTeam.DCGUI;
 public class DiagramC {
 	
 		public static void main(String[] args) {
-			Diagram diagram = new Diagram();
-			DiagramC_GUI mainGUI = new DiagramC_GUI(diagram);
+			DiagramC_GUI mainGUI = new DiagramC_GUI();
 			mainGUI.setDefaultCloseOperation(DiagramC_GUI.EXIT_ON_CLOSE);
 		    mainGUI.setVisible(true);
 	}
