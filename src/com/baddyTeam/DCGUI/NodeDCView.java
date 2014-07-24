f0480b91e4802363b399dbafed05161beef3b42f
Tobal Oval <X.zaning.X@gmail.com>
2014-07-24 00:31:39 -0400

    just "reorder"

--- src/com/baddyTeam/DCGUI/NodeDCView.java
+++ src/com/baddyTeam/DCGUI/NodeDCView.java
@@ -8,50 +8,41 @@ public abstract class NodeDCView implements Cloneable {
 	private Vector vector;
 	private int WIDTH;
 	private int HEIGHT;
-	
+
+    public NodeDCView(int x, int y,int WIDTH,int HEIGHT){
+        vector = new Vector(x, y);
+        this.HEIGHT = HEIGHT;
+        this.WIDTH = WIDTH;
+    }
+
+    @Override
+    public NodeDCView clone(){
+        try {
+            return (NodeDCView) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+
 	abstract void updateView(Graphics g);
 
 	abstract boolean imHere(double x, double y);
 	abstract NodeDC getNode();
-	
-	public NodeDCView(int x, int y,int WIDTH,int HEIGHT){
-		vector = new Vector(x, y);
-		this.HEIGHT = HEIGHT;
-		this.WIDTH = WIDTH;
-	}
+
 	
 	public int getXPosition(){
 		return vector.getX();
 	}
-	
+    public int getYPosition(){
+        return vector.getY();
+    }
 	public void setXPosition(int x){
 		vector.setX(x);
 	}
-	
-	public int getYPosition(){
-		return vector.getY();
-	}
-	
 	public void setYPosition(int y){
 		vector.setY(y);
-	}
-	
-	public void dragTo(int x, int y){
-		vector.setX(x - WIDTH/2);
-		vector.setY(y - HEIGHT/2);
-	}
-	
-	public Vector getVector(){
-		return vector;
-	}
-	
-	public NodeDCView clone(){
-		try {
-            return (NodeDCView) super.clone();
-        } catch (CloneNotSupportedException e) {        
-            e.printStackTrace();
-            throw new RuntimeException();
-        }
 	}
 
 	public void setWIDTH(int WIDTH) {
@@ -66,4 +57,13 @@ public abstract class NodeDCView implements Cloneable {
 	public int getHEIGHT(){
 		return this.HEIGHT;
 	}
+
+    public void dragTo(int x, int y){
+        vector.setX(x - WIDTH/2);
+        vector.setY(y - HEIGHT/2);
+    }
+
+    public Vector getVector(){
+        return vector;
+    }
 }
